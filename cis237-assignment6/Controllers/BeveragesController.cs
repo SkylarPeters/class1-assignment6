7b97ae9604fe17fa676c10a6de91bafe84bf6590
Skylar Peters <skylarpeters@ymail.com>
2021-04-27 15:56:34 -0400

    Add a Filter to the Beverage List.

--- cis237-assignment6/Controllers/BeveragesController.cs
+++ cis237-assignment6/Controllers/BeveragesController.cs
@@ -18,7 +18,71 @@ namespace cis237_assignment6.Controllers
         // GET: Beverages
         public ActionResult Index()
         {
-            return View(db.Beverages.ToList());
+            // Setup a variable to gold the beverages data.
+            DbSet<Beverage> BeveragesToFilter = db.Beverages;
+
+            // Setup some strings to hold the data that might be
+            // in the session. If there is nothing in the session
+            // we can still use these variables as a default value.
+            string filterName = "";
+            string filterMin = "";
+            string filterMax = "";
+            // Define a min and max for the price
+            decimal min = 0;
+            decimal max = 1000;
+
+            // Check to see if there is a value in the session,
+            // and if there is, assign it to the variable that
+            // we set up to hold the value.
+            if (!string.IsNullOrWhiteSpace(
+                (string)Session["session_name"]
+            ))
+            {
+                filterName = (string)Session["session_name"];
+            }
+
+            if (!string.IsNullOrWhiteSpace(
+                (string)Session["session_min"]
+            ))
+            {
+                filterMin = (string)Session["session_min"];
+                min = decimal.Parse(filterMin);
+            }
+
+            if (!string.IsNullOrWhiteSpace(
+            (string)Session["session_max"]
+            ))
+            {
+                filterMax = (string)Session["session_max"];
+                max = decimal.Parse(filterMax);
+
+            }
+
+            // Do the filter on the BeveragesToFilter Dataset.
+            // Use the where that we used before doing
+            // the last inclass, only this tume send in more
+            // lambda expressions to narrow it down further.
+            // Since we set up the default values for each of the
+            // filter parameters, min, max, and filterName, we
+            // can count on this always running with no errors.
+            IList<Beverage> finalFiltered = BeveragesToFilter.Where(
+                Beverage => Beverage.price >= min &&
+                Beverage.price <= max &&
+                Beverage.name.Contains(filterName)
+            ).ToList();
+
+            // Place the string representation of the values
+            // that are in the session into the viewbag so
+            // that they can be retrieved and displayed on the view.
+            ViewBag.filterName = filterName;
+            ViewBag.filterMin = filterMin;
+            ViewBag.filterMax = filterMax;
+
+
+            // Return the view with the filtered selection of cars.
+            return View(finalFiltered);
+
+            //return View(db.Beverages.ToList());
         }
 
         // GET: Beverages/Details/5
@@ -113,6 +177,31 @@ namespace cis237_assignment6.Controllers
             Beverage beverage = db.Beverages.Find(id);
             db.Beverages.Remove(beverage);
             db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Mark the method as Post since it is reached from a form submit
+        // Make sure to validate the antiforgery token.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Filter()
+        {
+            // Get the form data that we sent out of the request object.
+            // The string that is used as a key to get the data matches
+            // the name property of the form control
+            string name = Request.Form.Get("name");
+            string min = Request.Form.Get("min");
+            string max = Request.Form.Get("max");
+
+            // Now that we have the data pulled out from the 
+            // request object, let's put it into the session
+            // so that other methods can have access to it.
+            Session["session_name"] = name;
+            Session["session_min"] = min;
+            Session["session_max"] = max;
+
+
+            // Redirect to the index page
             return RedirectToAction("Index");
         }
 
